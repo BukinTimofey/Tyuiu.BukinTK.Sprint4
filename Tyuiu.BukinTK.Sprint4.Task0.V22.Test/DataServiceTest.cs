c829c7bc662f9dc7f3d28de5e3fbf1d4299e80dc
forengeit345 <timophey2907@gmail.com>
2025-11-01 12:53:52 +0500

    Add project files.

---
+++ Tyuiu.BukinTK.Sprint4.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BukinTK.Sprint4.Task0.V22.Lib;
+
+namespace Tyuiu.BukinTK.Sprint4.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            int res = ds.GetSumOddArrEl(numsArray);
+            int resAwait = 46;
+            Assert.AreEqual(resAwait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint4.Task0.V22/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BukinTK.Sprint4.Task0.V22.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] numsArray = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+
+        Console.WriteLine("Массив { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 }");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма нечетных чисел массива = " + ds.GetSumOddArrEl(numsArray));
+
+        Console.ReadKey();
+    }
+}
