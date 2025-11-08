5523565cd47c0f31dac27a90971c506844d186df
forengeit345 <timophey2907@gmail.com>
2025-11-08 22:09:54 +0500

    Task4

---
+++ Tyuiu.BukinTK.Sprint4.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BukinTK.Sprint4.Task4.V11.Lib;
+
+namespace Tyuiu.BukinTK.Sprint4.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5]
+            {
+                {5, 7, 7, 8, 5,},
+                {6, 5, 6, 8, 6,},
+                {7, 6, 8, 8, 5,},
+                {7, 6, 7, 8, 6,},
+                {7, 6, 7, 7, 5,},
+            };
+            int res = ds.Calculate(mas2);
+            int resAwait = 13;
+
+            Assert.AreEqual(resAwait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint4.Task4.V11/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.BukinTK.Sprint4.Task4.V11.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите кол-во строк в массиве:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите кол-во столбцов в массиве:");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, columns];
+
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите {i},{j} элемент массива: \t");
+                mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма нечетных элементов массива = " + ds.Calculate(mtrx));
+
+        Console.ReadKey();
+    }
+}
