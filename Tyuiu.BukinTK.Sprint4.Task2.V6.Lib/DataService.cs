3c02da489f3c505813a6cc3873c369fb3445bae3
forengeit345 <timophey2907@gmail.com>
2025-11-08 21:13:04 +0500

    Task2_2

--- Tyuiu.BukinTK.Sprint4.Task2.V6.Lib/DataService.cs
+++ Tyuiu.BukinTK.Sprint4.Task2.V6.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.BukinTK.Sprint4.Task2.V6.Lib
         public int Calculate(int[] array)
         {
             {
-                int sum = 0;
+                int sum = 1;
                 for (int i = 0; i <= array.Length - 1; i++)
                 {
                     if (array[i] % 2 != 0)
