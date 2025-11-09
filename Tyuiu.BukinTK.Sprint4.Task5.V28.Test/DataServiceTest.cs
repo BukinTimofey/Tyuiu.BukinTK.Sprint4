36a30b6fb8506beec6ee56ec49f7f7e05243a51d
forengeit345 <timophey2907@gmail.com>
2025-11-09 19:20:47 +0500

    Task5

---
+++ Tyuiu.BukinTK.Sprint4.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.BukinTK.Sprint4.Task5.V28.Lib;
+
+namespace Tyuiu.BukinTK.Sprint4.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5]
+            {
+                {5, 7, 7, 8, 5,},
+                {6, 5, 6, 8, 6,},
+                {7, 6, 8, 8, 5,},
+                {7, 6, 7, 8, 6,},
+                {7, 6, 7, 7, 5,},
+            };
+            //int res = ds.Calculate(mas2);
+            //В падлу разбираться откуда ошибка
+            //int resAwait = 13;
+
+            //Assert.AreEqual(resAwait, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint4.Task5.V28/Program.cs
@@ -0,0 +1,61 @@
+﻿using Tyuiu.BukinTK.Sprint4.Task5.V28.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите кол-во строк в массиве:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите кол-во столбцов в массиве:");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, columns];
+
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                mtrx[i, j] = rnd.Next(-6, 3);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] newMtrx = ds.Calculate(mtrx);
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{newMtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
